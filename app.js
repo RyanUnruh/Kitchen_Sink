12bce9b53ad88f91ad4acf852295e169ea00dcbe
student <student@Admins-iMac.local>
2020-01-14 15:50:00 -0600

    second

---
+++ app.js
@@ -0,0 +1,93 @@
+
+// const states = 50;
+// var sum = 5 + 4;
+// var age = 31;
+
+// var Charles = {
+//     age: 16
+//    };
+// var Abby = {
+//     age: 27
+//    };
+// var James = {
+//     age: 18
+//    };
+// var John = {
+//     age: 17
+//    };
+
+
+
+// // Say Hello
+// function sayHello() {
+//     alert("Hello World!")
+// };
+
+// sayHello();
+
+// Array of nam&Age-------------------
+
+// var person = [
+//     {name: "Jon", age: 13}, //0
+//     {name:"Tim", age: 25}, 
+//     {name: "Chase", age:11}, 
+//     {name: "Sue", age:24}, 
+//     {name: "Lane", age:5} //4
+// ];
+
+// function checkAge(){
+//     for(i = 0; i < person.length; i++){
+//        if (person[i].age < 21) {
+//            console.log("Sorry, " + person[i].name + ", you are not old enough to view this page!")
+//        } else {
+//            console.log("Hello, " + person[i].name + ", you may enter.")
+//        }
+//     }
+// };
+
+// checkAge(person);
+
+
+
+// Check age--------------------
+
+// function checkAge(age) {
+//     if (age < 21) {
+//         alert("Sorry " + name + ", you aren't old enough to view this page!")
+//     } else {
+//         alert("Older than 21")
+//     }
+// };
+
+// Veggies
+
+// var list = ['Carrot', 'Onion', 'Potato', 'corn', 'Avacado'];
+
+// for(var i = 0; i < list.length; i++) {
+//     console.log(list[i]);
+// };
+
+
+// Check age
+
+// checkAge(Abby.age);
+// checkAge(Charles.age);
+// checkAge(John.age);
+// checkAge(James.age);
+
+// 11.
+var str = "Hello World!!";
+var n = str.length;
+
+function getLength() {
+    if (n % 2 == 0) {
+        alert('The world is nice and even!')} else{
+        alert('The world is an odd place!')
+    }
+
+
+}
+
+getLength();
+
+
